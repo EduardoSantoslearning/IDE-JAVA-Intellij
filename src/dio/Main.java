9f57a56542d904144fe639d19745df2d4c62211e
Eduardo Santos <eduardo_san1999@hotmail.com>
2023-03-16 00:37:46 -0300

    first commit

---
+++ src/dio/Main.java
@@ -0,0 +1,15 @@
+package dio;
+
+import dio.model.Gato;
+
+public class Main {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 6;
+        System.out.println("Hello world!" + (a+b));*/
+    }
+}
